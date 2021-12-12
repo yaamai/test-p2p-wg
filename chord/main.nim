125f95952d2917df9584d5d420c3f8e161d069c7
yaamai <ya_amai@yahoo.co.jp>
2021-12-12 09:20:40 +0900

    tuple ref is useless ?

--- chord/main.nim
+++ chord/main.nim
@@ -7,6 +7,7 @@ type
     to: T
 
 proc contains[T](range: RingRange[T], value: T): bool =
+  echo "contains ", value, " in ", range
   if range.fr == range.to:
     return true
   if range.fr < range.to:
@@ -18,37 +19,37 @@ type
   Route[T] = tuple
     id: T
     
-  Node[T] = tuple
+  Node[T] = ref object
     id: T
     successor: Route[T]
     predecessor: Option[Route[T]]
 
 proc bootstrap[T](id: T): Node[T] =
-  (id: id, successor: (id, ), predecessor: none((T,)))
+  Node[T](id: id, successor: (id, ), predecessor: none((T,)))
 
 proc join[T](newid: T, id: T): Node[T] =
   let successor = id.get_communicator().find_successor(newid)
-  (id: newid, successor: (successor,), predecessor: none((T,)))
+  Node[T](id: newid, successor: (successor,), predecessor: none((T,)))
 
 proc stabilize[T](self: var Node[T]) =
-  echo ">>> stabilize", self
-  let pred = self.id.get_communicator().get_predecessor()
+  echo ">>> stabilize", self[]
+  let pred = self.successor.id.get_communicator().get_predecessor()
   if pred.isSome():
     let range = (fr: self.id, to: self.successor.id)
     if range.contains(pred.get().id):
       self.successor = pred.get()
   self.successor.id.get_communicator().notify(self.id)
-  echo "<<< stabilize", self
+  echo "<<< stabilize", self[]
 
-proc notify[T](self: var Node[T], id: T) =
-  echo ">>> notify", self
+proc notify[T](self: Node[T], id: T) =
+  echo ">>> notify", self[]
   if self.predecessor.isNone():
     self.predecessor = some((id, ))
   else:
     let range = (fr: self.predecessor.get().id, to: self.id)
     if range.contains(id):
       self.predecessor = some((id, ))
-  echo "<<< notify", self
+  echo "<<< notify", self[]
 
 proc find_successor[T](node: Node[T], id: T): T =
   let range = (fr: node.id, to: node.successor.id)
@@ -76,13 +77,13 @@ fn (n Node) find_successor(id ID) ID {
 
 type
   Comm[T] = tuple
-    n: ref Node[T]
+    n: Node[T]
 
 var
   m: Table[uint8, Node[uint8]]
 
 proc get_communicator(id: uint8): Comm[uint8] =
-  (n: (ref Node[uint8])(m[id]), )
+  (n: m[id], )
 
 proc is_element_of(id: uint8, f: uint8, t: uint8, fb: bool, tb: bool): bool = 
   true
@@ -123,6 +124,13 @@ m[0].stabilize()
 m[2] = join[uint8](2, 0)
 # echo m[2]
 m[0].stabilize()
+m[0].stabilize()
+m[2].stabilize()
 m[2].stabilize()
 
 m[1] = join[uint8](1, 0)
+
+echo "---"
+echo m[0][]
+echo m[1][]
+echo m[2][]
