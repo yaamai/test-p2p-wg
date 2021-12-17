ddebf6a0337969d4120359e1210ebee8491d0d9d
yaamai <ya_amai@yahoo.co.jp>
2021-12-17 00:37:25 +0000

    work 2peer ping

--- main.v
+++ main.v
@@ -20,14 +20,20 @@ fn bootstrap() ?wireguard.Device {
 }
 
 fn generate_peer_confg(mut dev wireguard.Device) ?JoinConfig {
+  // should be random or network unique
+  peer_addr := "10.163.0.2"
   public_key, private_key := wireguard.new_key()?.base64()
 
-  peer1 := wireguard.new_peer(key: public_key)?
-  dev.set_peer(peer1)
+  peer := wireguard.new_peer(key: public_key, allowed_ip: peer_addr)?
+  dev.set_peer(peer)
   dev.apply()?
+  netlink.add_if_route(peer_addr, 32, dev.get_index())?
 
   return JoinConfig {
     private_key: private_key,
+    tunnel_addr: peer_addr,
+    // TODO: query to netlink? or hold wireguard.Device or generate ip from id
+    remote_tunnel_addr: "10.163.0.1",
     remote_port: 43617,
     remote_public_key: dev.get_public_key(),
   }
@@ -37,6 +43,8 @@ fn generate_peer_confg(mut dev wireguard.Device) ?JoinConfig {
 struct JoinConfig {
 mut:
   private_key string
+  tunnel_addr string
+  remote_tunnel_addr string
   remote_addr string
   remote_port int
   remote_public_key string
@@ -46,10 +54,11 @@ fn do_join(p JoinConfig) ?wireguard.Device {
   mut dev := wireguard.new_device("sss0", true)?
   dev.set_private_key(p.private_key)
   dev.set_listen_port(43617)
-  dev.set_peer(wireguard.new_peer(key: p.remote_public_key, addr: p.remote_addr, port: p.remote_port)?)
+  dev.set_peer(wireguard.new_peer(key: p.remote_public_key, addr: p.remote_addr, port: p.remote_port, allowed_ip: p.remote_tunnel_addr)?)
   dev.apply()?
   netlink.set_interface_up(dev.get_index())?
-  netlink.add_interface_addr(dev.get_index(), "10.163.0.2", 32)?
+  netlink.add_interface_addr(dev.get_index(), p.tunnel_addr, 32)?
+  netlink.add_if_route(p.remote_tunnel_addr, 32, dev.get_index())?
 
   return dev
 }
@@ -78,8 +87,6 @@ fn main() {
   }
   action := os.args[1]
 
-  // TODO: configure allowed ip on peer
-  // TODO: append route ...
   if action == "bootstrap" {
     mut dev := bootstrap()?
   } else if action == "genpeer" {
