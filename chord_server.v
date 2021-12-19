725e63f4f12cf84ef06aef7ab0bea86bac8c4f60
yaamai <ya_amai@yahoo.co.jp>
2021-12-19 16:27:43 +0000

    fix http server routing

--- chord_server.v
+++ chord_server.v
@@ -3,6 +3,9 @@ import net.http
 import net.urllib
 import chord
 
+const empty = http.new_response(text: "", header: http.new_header(key: http.CommonHeader.content_length, value: "0"))
+const path_not_found = http.new_response(text: "path not found")
+
 struct ChordHandler {
 mut:
   node &chord.Node
@@ -10,19 +13,21 @@ mut:
 
 fn (mut h ChordHandler) handle(req http.Request) http.Response {
   url := urllib.parse(req.url) or { return http.Response{} }
-  return match url.path {
-    "/predecessor" { h.handle_get_predecessor(req, url) }
-    "/successor" { h.handle_get_successor(req, url) }
-    "/notify" { h.handle_notify(req, url) }
-    "/kvs/" { h.handle_query(req, url) }
-    "/kvs" { h.handle_store(req, url) }
-    else { http.Response{} }
+  resp := match true {
+    url.path.starts_with("/predecessor") { h.handle_get_predecessor(req, url) }
+    url.path.starts_with("/successor") { h.handle_get_successor(req, url) }
+    url.path.starts_with("/notify") { h.handle_notify(req, url) }
+    url.path.starts_with("/kvs/") { h.handle_query(req, url) }
+    url.path.starts_with("/kvs") { h.handle_store(req, url) }
+    else { path_not_found }
   }
+
+  return resp
 }
 
 fn (h ChordHandler) handle_get_predecessor(req http.Request, url urllib.URL) http.Response {
   if !h.node.has_predecessor {
-    return http.new_response(text: "", header: http.new_header(key: http.CommonHeader.content_length, value: "0"))
+    return empty
   }
   return http.new_response(text: h.node.predecessor)
 }
@@ -32,13 +37,13 @@ fn (h ChordHandler) handle_get_successor(req http.Request, url urllib.URL) http.
   if succ := h.node.find_successor(target) {
     return http.new_response(text: succ)
   }
-  return http.new_response(text: "", header: http.new_header(key: http.CommonHeader.content_length, value: "0"))
+  return empty
 }
 
 fn (mut h ChordHandler) handle_notify(req http.Request, url urllib.URL) http.Response {
   println("receive notify ${req.data}")
   h.node.notify(req.data)
-  return http.new_response(text: "", header: http.new_header(key: http.CommonHeader.content_length, value: "0"))
+  return empty
 }
 
 fn (h ChordHandler) handle_query(req http.Request, url urllib.URL) http.Response {
@@ -49,7 +54,7 @@ fn (h ChordHandler) handle_query(req http.Request, url urllib.URL) http.Response
   if val := h.node.query(names[2]) {
     return http.new_response(text: val)
   }
-  return http.new_response(text: "", header: http.new_header(key: http.CommonHeader.content_length, value: "0"))
+  return empty
 }
 
 fn (mut h ChordHandler) handle_store(req http.Request, url urllib.URL) http.Response {
@@ -58,9 +63,9 @@ fn (mut h ChordHandler) handle_store(req http.Request, url urllib.URL) http.Resp
     return http.new_response(text: "invalid path")
   }
   h.node.set(names[2], req.data) or {
-    return http.new_response(text: "", header: http.new_header(key: http.CommonHeader.content_length, value: "0"))
+    return empty
   }
-  return http.new_response(text: "", header: http.new_header(key: http.CommonHeader.content_length, value: "0"))
+  return empty
 }
 
 struct TestStore {
