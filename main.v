f5c75e484d4fab1c956536c0c60c841806e1e80b
yaamai <ya_amai@yahoo.co.jp>
2021-12-27 02:56:59 +0000

    test nat

--- main.v
+++ main.v
@@ -196,3 +196,52 @@ sudo ip netns exec siteA ip link set dev lo up
 sudo ip netns exec siteB ip link set dev lo up
 sudo ip netns exec siteC ip link set dev lo up
 */
+
+/*
+sudo ip netns add siteA
+sudo ip netns add siteB
+sudo ip netns add siteC
+sudo ip netns add siteD
+sudo ip netns add siteDl
+
+sudo ip link add vethA type veth peer name veth0
+sudo ip link set dev veth0 netns siteA
+sudo ip link add vethB type veth peer name veth0
+sudo ip link set dev veth0 netns siteB
+sudo ip link add vethC type veth peer name veth0
+sudo ip link set dev veth0 netns siteC
+sudo ip link add vethD type veth peer name veth0
+sudo ip link set dev veth0 netns siteD
+
+sudo ip link add inet0 type bridge
+sudo ip link set dev inet0 up
+sudo ip link set dev vethA master inet0 up
+sudo ip link set dev vethB master inet0 up
+sudo ip link set dev vethC master inet0 up
+sudo ip link set dev vethD master inet0 up
+
+sudo ip netns exec siteA ip addr add dev veth0 10.0.0.1/24
+sudo ip netns exec siteB ip addr add dev veth0 10.0.0.2/24
+sudo ip netns exec siteC ip addr add dev veth0 10.0.0.3/24
+sudo ip netns exec siteD ip addr add dev veth0 10.0.0.4/24
+sudo ip netns exec siteA ip link set dev veth0 up addr 5e:55:de:aa:e7:43
+sudo ip netns exec siteB ip link set dev veth0 up addr 5e:55:de:bb:e7:43
+sudo ip netns exec siteC ip link set dev veth0 up addr 5e:55:de:cc:e7:43
+sudo ip netns exec siteD ip link set dev veth0 up addr 5e:55:de:dd:e7:43
+sudo ip netns exec siteA ip link set dev lo up
+sudo ip netns exec siteB ip link set dev lo up
+sudo ip netns exec siteC ip link set dev lo up
+sudo ip netns exec siteD ip link set dev lo up
+sudo ip netns exec siteDl ip link set dev lo up
+
+sudo ip link add lanD0 type veth peer name veth0
+sudo ip link set dev lanD0 netns siteD
+sudo ip link set dev veth0 netns siteDl
+sudo ip netns exec siteD ip link set dev lanD0 up
+sudo ip netns exec siteD ip addr add dev lanD0 192.168.100.254/24
+sudo ip netns exec siteDl ip link set dev veth0 up
+sudo ip netns exec siteDl ip addr add dev veth0 192.168.100.1/24
+sudo ip netns exec siteDl ip route add default via 192.168.100.254
+sudo ip netns exec siteD iptables -t nat -I POSTROUTING -s 192.168.100.0/24 -o veth0 -j MASQUERADE --random
+
+*/
