fc67b6dcc75badde188aa74e84b7f8440fa68154
yaamai <ya_amai@yahoo.co.jp>
2021-12-28 01:58:34 +0000

    napt not work

--- main.v
+++ main.v
@@ -225,11 +225,11 @@ sudo ip link set dev vethC master inet0 up
 sudo ip link set dev vethD master inet0 up
 sudo ip link set dev vethE master inet0 up
 
-sudo ip netns exec siteA ip addr add dev veth0 10.0.0.1/24
-sudo ip netns exec siteB ip addr add dev veth0 10.0.0.2/24
-sudo ip netns exec siteC ip addr add dev veth0 10.0.0.3/24
-sudo ip netns exec siteD ip addr add dev veth0 10.0.0.4/24
-sudo ip netns exec siteE ip addr add dev veth0 10.0.0.5/24
+sudo ip netns exec siteA ip addr add dev veth0 100.0.0.1/24
+sudo ip netns exec siteB ip addr add dev veth0 100.0.0.2/24
+sudo ip netns exec siteC ip addr add dev veth0 100.0.0.3/24
+sudo ip netns exec siteD ip addr add dev veth0 100.0.0.4/24
+sudo ip netns exec siteE ip addr add dev veth0 100.0.0.5/24
 sudo ip netns exec siteA ip link set dev veth0 up addr 5e:55:de:aa:e7:43
 sudo ip netns exec siteB ip link set dev veth0 up addr 5e:55:de:bb:e7:43
 sudo ip netns exec siteC ip link set dev veth0 up addr 5e:55:de:cc:e7:43
@@ -251,7 +251,7 @@ sudo ip netns exec siteD ip addr add dev lanD0 192.168.100.254/24
 sudo ip netns exec siteDl ip link set dev veth0 up
 sudo ip netns exec siteDl ip addr add dev veth0 192.168.100.1/24
 sudo ip netns exec siteDl ip route add default via 192.168.100.254
-sudo ip netns exec siteD iptables -t nat -I POSTROUTING -s 192.168.100.0/24 -o veth0 -j MASQUERADE --random
+sudo ip netns exec siteD iptables -t nat -I POSTROUTING -o veth0 -j MASQUERADE --random
 
 sudo ip link add lanE0 type veth peer name veth0
 sudo ip link set dev lanE0 netns siteE
@@ -261,5 +261,5 @@ sudo ip netns exec siteE ip addr add dev lanE0 192.168.200.254/24
 sudo ip netns exec siteEl ip link set dev veth0 up
 sudo ip netns exec siteEl ip addr add dev veth0 192.168.200.1/24
 sudo ip netns exec siteEl ip route add default via 192.168.200.254
-sudo ip netns exec siteE iptables -t nat -I POSTROUTING -s 192.168.200.0/24 -o veth0 -j MASQUERADE --random
+sudo ip netns exec siteE iptables -t nat -I POSTROUTING -o veth0 -j MASQUERADE --random
 */
